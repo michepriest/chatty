37de1cc217c0c233b8419dce4b3cf9db9c8f1610
michepriest <miche.priest@gmail.com>
2018-10-30 21:50:45 +0000

    set initial state, pass currentUser using props, pass messages as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList messages={ this.state.messages }/>
+        <ChatBar currentUser={ this.state.currentUser } />
+      </div>
     );
   }
 }
 export default App;
+
+
---
+++ src/MessageList.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    const allTheMessages = this.props.messages.map((message) => {
+    return <Message message={ message } />
+  });
+  
+    return (
+      <main className="messages">
+        { allTheMessages }
+      </main>
+    );
+  }
+}
+export default MessageList;

d1263bb30d4af8bc83581d3b6213eedfa97f47ab
michepriest <miche.priest@gmail.com>
2018-11-05 04:29:24 +0000

    Display how many users online

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
-// import { url } from 'inspector';
-
 class App extends Component {
   constructor(props) { // use constructor for initializing state and binding methods
     super(props); // mandatory for constructors, must be called before any other state otherwise this.props will be undefined
     this.state = { // initializing state, we have to use 'state' for data that is going to change
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: []
+      messages: [],
+      onlineUsers: 0
     };
     this.socket = new WebSocket('ws://localhost:3001');
     this.onNewMessage = this.onNewMessage.bind(this);
@@ -32,24 +31,24 @@ class App extends Component {
       switch(newMsg.type) {
         case "changeUser":
           let messagesWithNewMessage = this.state.messages.concat(newMessage); 
-          this.setState({ messages: messagesWithNewMessage }) ;
+          this.setState({ messages: messagesWithNewMessage });
           break;
         case "changeMessage":
-        console.log("message after message change", newMsg)
           let messagesWithNewMessages = this.state.messages.concat(newMessage); 
-          this.setState({ messages: messagesWithNewMessages }) ;
-      }
-      
-      
+          this.setState({ messages: messagesWithNewMessages });
+          break;
+        case "connectedClients":
+          let userCount = newMsg.count;
+          this.setState({
+            onlineUsers: userCount
+          })
+          break;
+      } 
     }
-      // const updateUsername = this.state.username.concat(newUsername)
-      // this.setState({ username: updateUsername })
   }
 
   onNewMessage(content) { // sends message to the server
     const newMessage = { type: "changeMessage", currentUser: this.state.currentUser.name, content }; // assigns id to new message
-    // const messagesWithNewMessage = this.state.messages.concat(newMessage); // adds new message to message list
-    // this.setState({ messages: messagesWithNewMessage }) // displays all messages
     this.socket.send(JSON.stringify(newMessage))
   }
 
@@ -66,11 +65,12 @@ class App extends Component {
     this.setState({ currentUser: {name: newUsername}});
   }
 
-  render() { // renders App
+  render() { 
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
+          <span className = "counter">{this.state.onlineUsers} users online</span>
         </nav>
         <MessageList messages={ this.state.messages } /> 
         <ChatBar currentUser={ this.state.currentUser } onNewMessage = { this.onNewMessage } onNewUsername = { this.onNewUsername } />
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -11,23 +11,15 @@ class ChatBar extends Component { // username and new message component
   handleMessageKeypress(event) {
     if(event.key === 'Enter') {
       this.props.onNewMessage(event.target.value);
-      // console.log("EVENT TARGET CLASSNAME", event.target.className)
-      // console.log("EVENT TARGET VALUE", event.target.value)
     }
   }
   handleUsernameKeypress(event) {
     if(event.key === 'Enter' ) {
       this.props.onNewUsername(event.target.value);
-      // console.log("THIS IS MY NEW USERNAME!!!", event.target.value)
     }
   }
 
-
-
-
-
   render() {
-    console.log("THIS HANDLE MESSAGE KEYPRESS", this.handleMessageKeypress)
     return (
       <footer className="chatbar">
         <input className="chatbar-username" onKeyPress={ this.handleUsernameKeypress } placeholder={ this.props.currentUser.name } />
